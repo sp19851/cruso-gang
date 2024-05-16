b15a55de3d56ca24d728e402273f346b369f0e2e
sp19851@gmail.com <sp19851@gmail.com>
2024-05-16 16:02:56 +0200

    sql

--- cruso_sellers.sql
+++ cruso_sellers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Май 16 2024 г., 11:34
+-- Время создания: Май 16 2024 г., 16:02
 -- Версия сервера: 10.4.32-MariaDB
 -- Версия PHP: 8.0.30
 
@@ -28,9 +28,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cruso_sellers` (
-  `id` varchar(155) NOT NULL,
+  `id` int(155) NOT NULL,
+  `id_seller` varchar(155) NOT NULL,
   `item` varchar(255) NOT NULL,
   `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `cruso_sellers_accounts`
+--
+
+CREATE TABLE `cruso_sellers_accounts` (
+  `id` int(11) NOT NULL,
+  `id_seller` varchar(155) NOT NULL,
+  `account` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
@@ -41,7 +54,31 @@ CREATE TABLE `cruso_sellers` (
 -- Индексы таблицы `cruso_sellers`
 --
 ALTER TABLE `cruso_sellers`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_seller` (`id_seller`,`item`);
+
+--
+-- Индексы таблицы `cruso_sellers_accounts`
+--
+ALTER TABLE `cruso_sellers_accounts`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_seler` (`id_seller`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `cruso_sellers`
+--
+ALTER TABLE `cruso_sellers`
+  MODIFY `id` int(155) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `cruso_sellers_accounts`
+--
+ALTER TABLE `cruso_sellers_accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
